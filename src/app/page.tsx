1f71ad285e214a1b5971a76a613f4872508ec679
Kevin Nicolas <kncalderong@gmail.com>
2023-07-12 11:14:35 -0500

    feat: header added across the pages using layout and _app component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { prisma } from '@/db'
 
 export default async function Home() {
-  const jobs = await prisma.job.findMany({
+  /* const jobs = await prisma.job.findMany({
     include: {
       languages: true,
     },
-  }) //this is called on the server because the whole component is server-side
+  }) */ //this is called on the server because the whole component is server-side rendering
 
-  return <main>sdafasdf</main>
+  return (
+    <main className='font-league-spartan font-medium text-very-dark-grayish-cyan'>
+      Senior Frontend Developer
+    </main>
+  )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='w-full h-[156px] bg-[url("/bg-header-mobile.svg")] bg-desaturated-dark-cyan'></header>
+  )
+}
+
+export default Header

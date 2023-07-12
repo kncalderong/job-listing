94292d8b04b7fca9ee12b448ec03bb901ab26c42
Kevin Nicolas <kncalderong@gmail.com>
2023-07-12 10:21:26 -0500

    style: basic style configurations for design added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'desaturated-dark-cyan': 'hsl(180, 29%, 50%)',
+        'light-bg-grayish-cyan': 'hsl(180, 52%, 96%)',
+        'light-filter-grayish-cyan': 'hsl(180, 31%, 95%)',
+        'dark-grayish-cyan': 'hsl(180, 8%, 52%)',
+        'very-dark-grayish-cyan': 'hsl(180, 14%, 20%)',
+      },
+      fontFamily: {
+        'league-spartan': ['League Spartan', 'sans-serif'],
+        agdashima: ['Agdasima', 'sans-serif'],
+      },
     },
   },
   plugins: [],

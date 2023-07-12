f9bc383fcfbcfec6bf3e243b0bf0add9a712634c
Kevin Nicolas <kncalderong@gmail.com>
2023-07-11 20:34:36 -0500

    feat: database populated with some jobs according to frontEndMentor challenge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,11 @@
 import { prisma } from '@/db'
 
 export default async function Home() {
-  /* const jobCreated = await prisma.job.create({
-    data: {
-      company: 'Photosnap',
-      logo: './images/photosnap.svg',
-      new: true,
-      featured: true,
-      position: 'Senior Frontend Developer',
-      role: 'Frontend',
-      level: 'Senior',
-      postedAt: '1d ago',
-      contract: 'Full Time',
-      location: 'USA Only',
-      languages: {
-        create: [{ name: 'HTML' }, { name: 'CSS' }, { name: 'JavaScript' }],
-      },
-      tools: [],
+  const jobs = await prisma.job.findMany({
+    include: {
+      languages: true,
     },
-  }) */
+  }) //this is called on the server because the whole component is server-side
 
-  /* const languagesCreated = await prisma.lenguages.createMany({
-    data: [{
-      name: 'Python'
-    }]
-  }) */
-  /* await prisma.lenguages.deleteMany()
-  await prisma.job.deleteMany() */
-  const jobs = await prisma.job.findMany() //this is called on the server because the whole component is server-side
-
-  console.log('jobs', jobs)
-
-  return <main>Hello wolrd</main>
+  return <main>sdafasdf</main>
 }
---
+++ utils/populate.ts
@@ -0,0 +1,220 @@
+const one = {
+  company: 'Photosnap',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/b8bed2f5-829d-444c-a757-651194a2347c',
+  new: true,
+  featured: true,
+  position: 'Senior Frontend Developer',
+  role: 'Frontend',
+  level: 'Senior',
+  postedAt: '1d ago',
+  contract: 'Full Time',
+  location: 'USA Only',
+  languages: {
+    connect: [
+      { id: '27f3e1ec-379d-4cee-a948-993e53cfc12c' },
+      { id: 'b96cff25-083b-4338-8024-2bf3272c02f7' },
+      { id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' },
+    ],
+  },
+  tools: [],
+}
+
+const two = {
+  company: 'Manage',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/75b8956a-0ba0-478c-9d63-4a5f126c5c0e',
+  new: true,
+  featured: true,
+  position: 'Fullstack Developer',
+  role: 'Fullstack',
+  level: 'Mid',
+  postedAt: '1d ago',
+  contract: 'Part Time',
+  location: 'Remote',
+  languages: {
+    connect: [
+      {
+        id: '63f130c5-2e63-44b3-b25b-b4918ef590c2',
+      },
+    ],
+  },
+  tools: ['React'],
+}
+
+const three = {
+  company: 'Account',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/69d61573-38ee-4105-be8d-3106315fa3c4',
+  new: true,
+  featured: false,
+  position: 'Junior Frontend Developer',
+  role: 'Frontend',
+  level: 'Junior',
+  postedAt: '2d ago',
+  contract: 'Part Time',
+  location: 'USA Only',
+  languages: {
+    connect: [
+      {
+        id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87',
+      },
+    ],
+  },
+  tools: ['React', 'Sass'],
+}
+
+const four = {
+  company: 'MyHome',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/74cdefaf-0c6d-4689-aa85-49d76ee41ac5',
+  new: false,
+  featured: false,
+  position: 'Junior Frontend Developer',
+  role: 'Frontend',
+  level: 'Junior',
+  postedAt: '5d ago',
+  contract: 'Contract',
+  location: 'USA Only',
+  languages: {
+    connect: [
+      {
+        id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87',
+      },
+      { id: 'b96cff25-083b-4338-8024-2bf3272c02f7' },
+    ],
+  },
+  tools: [],
+}
+
+const five = {
+  company: 'Loop Studios',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/8915db89-541f-4659-bc5c-fe69b1bde6ab',
+  new: false,
+  featured: false,
+  position: 'Software Engineer',
+  role: 'Fullstack',
+  level: 'Mid',
+  postedAt: '1w ago',
+  contract: 'Full Time',
+  location: 'Worldwide',
+  languages: {
+    connect: [{ id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' }],
+  },
+  tools: ['Ruby', 'Sass'],
+}
+
+const six = {
+  company: 'FaceIt',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/7da8aa22-e16e-4f4e-935b-6185d35a27c4',
+  new: false,
+  featured: false,
+  position: 'Junior Backend Developer',
+  role: 'Backend',
+  level: 'Junior',
+  postedAt: '2w ago',
+  contract: 'Full Time',
+  location: 'UK Only',
+  languages: {
+    connect: [{ id: 'a805c24c-6afb-46e4-82d0-851c61f34e33' }],
+  },
+  tools: ['RoR'],
+}
+
+const seven = {
+  company: 'Shortly',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/514f79e1-e565-43b1-af4c-94ddbfed4ec9',
+  new: false,
+  featured: false,
+  position: 'Junior Developer',
+  role: 'Frontend',
+  level: 'Junior',
+  postedAt: '2w ago',
+  contract: 'Full Time',
+  location: 'Worldwide',
+  languages: {
+    connect: [
+      { id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' },
+      { id: '27f3e1ec-379d-4cee-a948-993e53cfc12c' },
+    ],
+  },
+  tools: ['Sass'],
+}
+
+const eight = {
+  company: 'Insure',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/6fc0d5bc-a508-44f4-8c48-d015a0c8351e',
+  new: false,
+  featured: false,
+  position: 'Junior Frontend Developer',
+  role: 'Frontend',
+  level: 'Junior',
+  postedAt: '2w ago',
+  contract: 'Full Time',
+  location: 'USA Only',
+  languages: {
+    connect: [{ id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' }],
+  },
+  tools: ['Vue', 'Sass'],
+}
+
+const nine = {
+  company: 'Eyecam Co.',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/068e24df-ded7-4a9b-9be4-f9b8f12cc80b',
+  new: false,
+  featured: false,
+  position: 'Full Stack Engineer',
+  role: 'Fullstack',
+  level: 'Mid',
+  postedAt: '3w ago',
+  contract: 'Full Time',
+  location: 'Worldwide',
+  languages: {
+    connect: [
+      { id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' },
+      { id: '63f130c5-2e63-44b3-b25b-b4918ef590c2' },
+    ],
+  },
+  tools: ['Django'],
+}
+
+const ten = {
+  company: 'The Air Filter Company',
+  logo: 'https://github.com/kncalderong/job-listing/assets/80135017/3f37925d-eb14-4287-88b7-4907465e038f',
+  new: false,
+  featured: false,
+  position: 'Front-end Dev',
+  role: 'Frontend',
+  level: 'Junior',
+  postedAt: '1mo ago',
+  contract: 'Part Time',
+  location: 'Worldwide',
+  languages: {
+    connect: [{ id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87' }],
+  },
+  tools: ['React', 'Sass'],
+}
+
+const languages = [
+  {
+    id: '63f130c5-2e63-44b3-b25b-b4918ef590c2',
+    name: 'Python',
+    jobId: null,
+  },
+  {
+    id: '27f3e1ec-379d-4cee-a948-993e53cfc12c',
+    name: 'HTML',
+    jobId: null,
+  },
+  {
+    id: 'b96cff25-083b-4338-8024-2bf3272c02f7',
+    name: 'CSS',
+    jobId: null,
+  },
+  {
+    id: 'fb26eb9c-4713-4e1a-860b-7027c1024d87',
+    name: 'JavaScript',
+    jobId: null,
+  },
+  {
+    id: 'a805c24c-6afb-46e4-82d0-851c61f34e33',
+    name: 'Ruby',
+    jobId: null,
+  },
+]
